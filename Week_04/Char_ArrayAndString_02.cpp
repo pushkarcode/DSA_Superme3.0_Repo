7356b86c66149e02d99a3e088cb93f8174f5221b
Pushakr <pushkarsharma9131@gmail.com>
2024-07-11 23:49:12 +0530

    charArray

--- Week_04/Char_ArrayAndString_02.cpp
+++ Week_04/Char_ArrayAndString_02.cpp
@@ -1,8 +1,90 @@
 #include <iostream>
 using namespace std;
+
+string removeAdcjent(string str, int n)
+{
+    string ans = "";
+    for (int i = 0; i < n; i++)
+    {
+        char currentValue = str[i];
+        if (ans.empty() || currentValue != ans.back())
+        {
+            ans.push_back(currentValue);
+        }
+        else if (currentValue == ans.back())
+        {
+            ans.pop_back();
+        }
+    }
+    return ans;
+}
+
+string removeOccrancer(string str, string pattern)
+{
+    // ek loop chalo jab full string mai patteren mila
+    while (str.find(pattern) != string::npos)
+    {
+        // str string mai erase karo earse function 2 argument (start
+        // index) - size of delete
+
+        str.erase(str.find(pattern), pattern.length());
+    }
+    return str;
+}
+bool checkPla(string str, int s, int e)
+{
+    while (s <= e)
+    {
+        if (str[s] != str[e])
+        {
+            return false;
+        }
+        else
+        {
+            s++, e--;
+        }
+    }
+    return true;
+}
+
 int main()
 {
 
-    
+    // string str = "daabcbaabcbc";
+    // int n = str.length();
+    // string pattern = "abc";
+    // string s = removeAdcjent(str, n);
+    // string s = removeOccrancer(str,  pattern);
+
+    //! vaild paindrom 2 refer leetcode for this
+
+    // string str = "abcasdfgvsd";
+    // int n = str.length();
+    // int i = 0;
+    // int j = n - 1;
+    // while (i <= j)
+    // {
+    //     // same
+    //     if (str[i] == str[j])
+    //     {
+    //         i++, j--;
+    //     }
+    //     // diffrent
+    //     else
+    //     {
+    //         bool ansOne = checkPla(str, i + 1, j);
+    //         bool ansTwo = checkPla(str, i, j - 1);
+    //         bool finalAns = ansOne || ansTwo;
+    //         // return finalAns;
+    //         cout << finalAns << endl;
+    //         break;
+    //     }
+    // } 
+
+    // cout << true << endl;
+
+    //?  Palindromic Substrings leetcode refer
+
+    // cout << s << endl;
     return 0;
 }
---
+++ Week_04/Char_ArrayAndString_03.cpp
@@ -0,0 +1,31 @@
+#include <iostream>
+#include <unordered_map>
+using namespace std;
+int main()
+{
+    //! Decode the message
+    string str = "the quick brown fox jumps over the lazy dog";
+    string message = "vkbs bs t suepuv";
+
+    // 2 step process
+
+    //? Step-1: Create a map of the alphabets
+    unordered_map<char, char> mapping;
+    char spaceCharacter = ' ';
+    mapping[spaceCharacter] = spaceCharacter;
+
+    char start = 'a';
+    for (int i = 0; i < str.length(); i++)
+    {
+        // if mapping is not assign
+        if (mapping.find(str[i]) == mapping.end())
+        {
+            mapping[str[i]] = start;
+            start++;
+        }
+    }
+
+    //* step -2 
+
+    return 0;
+}
