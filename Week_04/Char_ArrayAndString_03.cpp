0cb99ce033ebd3a546e079c603b735290dc78a5c
Pushakr <pushkarsharma9131@gmail.com>
2024-07-13 00:18:27 +0530

    add

--- Week_04/Char_ArrayAndString_03.cpp
+++ Week_04/Char_ArrayAndString_03.cpp
@@ -3,29 +3,41 @@
 using namespace std;
 int main()
 {
-    //! Decode the message
-    string str = "the quick brown fox jumps over the lazy dog";
-    string message = "vkbs bs t suepuv";
+    //! Decode the message---------------------------
+    // string str = "the quick brown fox jumps over the lazy dog";
+    // string message = "vkbs bs t suepuv";
 
-    // 2 step process
+    // // 2 step process
 
-    //? Step-1: Create a map of the alphabets
-    unordered_map<char, char> mapping;
-    char spaceCharacter = ' ';
-    mapping[spaceCharacter] = spaceCharacter;
+    // //? Step-1: Create a map of the alphabets
+    // unordered_map<char, char> mapping;
+    // char spaceCharacter = ' ';
+    // mapping[spaceCharacter] = spaceCharacter;
 
-    char start = 'a';
-    for (int i = 0; i < str.length(); i++)
-    {
-        // if mapping is not assign
-        if (mapping.find(str[i]) == mapping.end())
-        {
-            mapping[str[i]] = start;
-            start++;
-        }
-    }
+    // char start = 'a';
+    // for (int i = 0; i < str.length(); i++)
+    // {
+    //     // if mapping is not assign
+    //     if (mapping.find(str[i]) == mapping.end())
+    //     {
+    //         mapping[str[i]] = start;
+    //         start++;
+    //     }
+    // }
 
-    //* step -2 
+    // //* step -2  decode the message
+    // string decodemessage = "";
+    // for (int i = 0; i < message.length(); i++)
+    // {
+    //     char mappedcharacter = mapping[message[i]];
+    //     decodemessage.push_back(mappedcharacter);
+    // }
+
+    // cout << "Decoded Message: " << decodemessage << endl;
+
+    //! Minimum Amount of Time to Collect Garbage------------
+
+    
 
     return 0;
 }
