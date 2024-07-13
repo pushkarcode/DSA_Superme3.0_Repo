f9f9396d73847c395d64bb9ca47b1cd1999ef216
Pushakr <pushkarsharma9131@gmail.com>
2024-07-13 23:47:15 +0530

    char_arrays_completd

--- Week_04/Char_ArrayAndString_03.cpp
+++ Week_04/Char_ArrayAndString_03.cpp
@@ -1,6 +1,43 @@
 #include <iostream>
 #include <unordered_map>
+#include <algorithm>
+#include <vector>
 using namespace std;
+
+string ordercopy;
+
+bool cmp(char a, char b)
+{
+
+    // retru true means a >b
+    //    ease b >a;
+
+    return (ordercopy.find(a) < ordercopy.find(b));
+}
+
+void normalize(string &str)
+{
+    char start = 'a';
+    unordered_map<char, char> mapping;
+
+    for (int i = 0; i < str.length(); i++)
+    {
+        // find if mapping is already exsist or not
+        if (mapping.find(str[i]) == mapping.end())
+        {
+            mapping[str[i]] = start;
+            start++;
+        }
+    }
+
+    // and that chage actual string mee to abb
+    for (int i = 0; i < str.length(); i++)
+    {
+        char changevalu = mapping[str[i]];
+        str[i] = changevalu;
+    }
+}
+
 int main()
 {
     //! Decode the message---------------------------
@@ -37,7 +74,64 @@ int main()
 
     //! Minimum Amount of Time to Collect Garbage------------
 
-    
+    // string garbage[4] = {"G", "P", "GP", "GG"};
+    // int time[4] = {2, 4, 3};
+
+    // int pickP = 0;
+    // int travelP = 0;
+    // int lastHpuseP = 0;
+
+    // int pickM = 0;
+    // int travelM = 0;
+    // int lastHpuseM = 0;
+
+    // int pickG = 0;
+    // int travelG = 0;
+    // int lastHpuseG = 0;
+
+    // for (int i = 0; i < 4; i++)
+    // {
+    //    int currentHouse = garbage[i];
+    //    for (int j = 0; j < currentHouse.size(); j++)
+    //    {
+    //     /* code */
+    //    }
+
+    // }
+
+    //! Custom sort string
+
+    // string str = "abcd";
+    // string order = "cba";
+    // ordercopy = order;
+
+    // sort(str.begin(), str.end(), cmp);
+
+    // cout << "orderde string :" << str << endl;
+
+    //! find and replace the patteern;
+
+    vector<string> words = {"eff", "dfg", "llm", "dff"};
+    string pattern = "abb";
+
+    vector<string> ans;
+    // step A normalize pattern
+    normalize(pattern);
+    // step B words ki list mai haar ek word ko normalize karna hai
+    for (int i = 0; i < words.size(); i++)
+    {
+        string currWord = words[i];
+        normalize(currWord);
+        if (currWord.compare(pattern) == 0)
+        {
+            ans.push_back(words[i]);
+        }
+    }
+
+    for (int i = 0; i < ans.size(); i++)
+    {
+        cout << ans[i] << " ";
+    }
 
     return 0;
 }
---
+++ Week_04/Dobout_class.cpp
@@ -0,0 +1,13 @@
+#include <iostream>
+using namespace std;
+int main()
+{
+
+    char arr[4] = {};
+    cout<<arr;
+
+
+    //revison of all 3 class of char arrays questions
+
+    return 0;
+}
