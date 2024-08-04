913ddf7c99e9cbcc607a7f756e3573c9e92d5306
Pushakr <pushkarsharma9131@gmail.com>
2024-08-04 17:48:24 +0530

    Pointer&Math Completed

--- Week_05/Debug.cpp
+++ Week_05/Debug.cpp
@@ -1,6 +1,57 @@
 #include <iostream>
 using namespace std;
-int main()
+// void SieveOfEratosthenes(int n)
+// {
+//     vector<bool> prime(n+1, false);
+
+//     for (int p = 2; p * p <= n; p++) {
+//             for (int i = p * p; i <= n; i += p)
+//                 prime[i] = true;
+//         }
+//     }
+
+//     for (int p = 0; p <= n; p++)
+//         if (prime[p])
+//             cout << p << " ";
+// }
+
+bool isPrime(int n)
 {
-    return 0;
+    if (n == 1)
+        return false;
+
+    for (int i = 2; i <= n / 2; i++)
+        if (n % i == 0)
+            return false;
+
+    return true;
 }
+// int main()
+// {
+//     // cout << isPrime(5);
+
+//     return 0;
+// }
+
+// int main()
+// {
+//     int a = 5;
+//     int *ptr = &a;
+//     // ptr = &a;
+//     cout << *ptr << endl;
+//     return 0;
+// }
+
+#include <iostream>
+using namespace std;
+
+// int main()
+// {
+//     int p = 5;
+//     int *q = &p;
+//     p++;
+//     cout << p << endl;
+//     return 0;
+// }
+
+
