5666c26570b46208727ddf78e2a8cdce5a68d8d6
Pushakr <pushkarsharma9131@gmail.com>
2024-08-18 18:20:25 +0530

    change

--- Week_06/Recursion/Recursion_class_02.cpp
+++ Week_06/Recursion/Recursion_class_02.cpp
@@ -1,6 +1,37 @@
 #include <iostream>
+#include <vector>
 using namespace std;
 int main()
 {
+
+    // int arr[] = {1,2,3,4,,5}
+
+    vector<int> arr = {2, 4, -2, 7, -9};
+
+    // int n = arr.size();
+
+    // int low = 0, high = n - 1;
+
+    // while (low <= high)
+    // {
+    //     if (arr[low] < 0)
+    //     {
+    //         low++;
+    //     }
+    //     else if (arr[high] > 0)
+    //     {
+    //         high--;
+    //     }
+    //     else
+    //     {
+    //         swap(arr[low], arr[high]);
+    //     }
+    // }
+
+    // for (auto &&i : arr)
+    // {
+    //     cout << i << " ";
+    // }
+
     return 0;
 }
