9b6806186ec51d67e5b0bc8325060f7066a08c7d
Pushakr <pushkarsharma9131@gmail.com>
2024-09-20 23:45:15 +0530

    not

--- Week_03/Home_Works/Find-K_closest_elem.cpp
+++ Week_03/Home_Works/Find-K_closest_elem.cpp
@@ -4,10 +4,7 @@ using namespace std;
 int main()
 {
   //!-------------k-closest element------------
-  // int arr[] = {1, 2, 3};
-  //! use array insted of vector
-  vector<int> arr = {1, 2, 3, 4, 5};
-  //for the 
+ //? ya to karunga ya chood dunga
 
 
   
