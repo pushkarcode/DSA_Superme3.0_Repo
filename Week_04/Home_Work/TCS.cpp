a9c8b94c11c80d3b7d1408ce989ad24cd4f554f4
Pushakr <pushkarsharma9131@gmail.com>
2024-10-02 23:43:03 +0530

    change

---
+++ Week_04/Home_Work/TCS.cpp
@@ -0,0 +1,23 @@
+// #include <iostream>
+// #include <unordered_map>
+// using namespace std;
+// int main()
+// {
+//     string s = "tiger";
+//     unordered_map<int, char> match;
+//     for (int i = 0; i < s.length(); i++)
+//     {
+//         match[s[i]] = s[i];
+//     }
+//     string result = "ti";
+//   for (int i = 0; i < result.length(); i++)
+//   {
+//     for(const auto &pair : match) {
+//         if(pair.second == s[i]) {
+//             pair.first = 0;
+//         }
+//     }   
+//   }
+  
+
+// }
