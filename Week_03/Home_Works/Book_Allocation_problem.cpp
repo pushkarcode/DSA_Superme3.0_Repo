61992a720b2a2c85f8fb5e734ad39ffebee8b396
Pushakr <pushkarsharma9131@gmail.com>
2024-11-05 23:44:02 +0530

    change

--- Week_03/Home_Works/Book_Allocation_problem.cpp
+++ Week_03/Home_Works/Book_Allocation_problem.cpp
@@ -7,7 +7,7 @@ int main()
   // karna hei  aaj to  karunga tfhgrsd sdmgfe
   // geek for geek problem allocated books
   vector<int> arr = {1, 2, 3, 4, 5};
- //! hamrae sath 
+ //! hamrae sath  hofa yeha 
 
   return 0;
 }
