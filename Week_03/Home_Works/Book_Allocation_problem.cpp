45f92d4012f194053c92b79ca4e2ee305e7030fb
Pushakr <pushkarsharma9131@gmail.com>
2024-11-07 20:58:20 +0530

    Make_bool _alloocation

--- Week_03/Home_Works/Book_Allocation_problem.cpp
+++ Week_03/Home_Works/Book_Allocation_problem.cpp
@@ -1,14 +1,1 @@
-#include <iostream>
-#include <vector>
-using namespace std;
-int main()
-{
-
-  // karna hei  aaj to  karunga tfhgrsd sdmgfe
-  // geek for geek problem allocated books
-  vector<int> arr = {1, 2, 3, 4, 5};
- //! hamrae sath  hofa yeha sdfb 
- dfgdkmg sdfgv
-
-  return 0;
-}
+code 
