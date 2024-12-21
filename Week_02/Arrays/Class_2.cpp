1219c748d20d6a9cac44dea272ae6ed2108544aa
Pushakr <pushkarsharma9131@gmail.com>
2024-12-21 23:39:35 +0530

    data

--- Week_02/Arrays/Class_2.cpp
+++ Week_02/Arrays/Class_2.cpp
@@ -249,7 +249,7 @@ pair<int, int> checkTwoSum(int arr[], int n, int target)
     }
     // agar aap yha tak pohoch gye
     // iska matlab, koi bhi pair target k equal nahi mila
-    //calo chaneg aa gya sdfb sffs  sdfhg sdfhdnb sdgd sdgh wedf sdfgsdfb
+    //calo chaneg aa gya sdfb sffs  sdfhg sdfhdnb sdgd sdgh wedf sdfgsdfb sdfhg sdhdgs
     // iska matlab return false karo
     return ans;
 }
